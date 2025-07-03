00836f2001587ae7149ac2da04f7da4f18aa9282
rafaelmartinsch <rafaelmartinsch@gmail.com>
2025-07-02 21:14:39 -0400

    Atulizar js de importação para global

--- js/layout.js
+++ js/layout.js
@@ -1,14 +1,14 @@
 
 document.addEventListener("DOMContentLoaded", ()=> {
 
-    fetch("./header.html")
+    fetch("../header.html")
         .then((response)=>response.text())
         .then((data)=>{
             document.getElementById("header").innerHTML=data
         })
         .catch((error) => console.error("Erro ao carregar o header", error))
 
-    fetch("./footer-v2.html")
+    fetch("../footer-v2.html")
         .then((response)=>response.text())
         .then((data)=>{
             document.getElementById("footer").innerHTML=data
